96542c3d1c9b7c0ca92e3bbc5b98f44dc5cc462a
Jose Armas <armasjose606@gmail.com>
2023-07-15 23:31:17 -0400

    Perf: change uncontrolled to controlled forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { useRef } from 'react'
 import './App.css'
 import { Movies } from './components/movies.jsx'
 import { useMovies } from './hooks/useMovies.js'
+import { useSearch } from './hooks/useSearch'
+
 
 function App() {
-
-  const query = useRef('')
-  const newQuery = query.current.value
-  const { movies, getMovies } = useMovies(newQuery)
+  const { query, setQuery, error } = useSearch()
+  const { movies, getMovies } = useMovies({query})
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies()
-    if (query === '') {
-      return
-    } 
+    getMovies({query})
+  }
+
+  const handleChange = (event) => {
+    const newQuery = event.target.value
+    setQuery(newQuery)
   }
 
   return (
@@ -23,9 +24,10 @@ function App() {
       <header >
         <h1>Movie Search Engine</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input type='text' name='query' ref={query} />
+          <input onChange={handleChange} value={query} name='query' />
           <button  type='submit' value='query'>Search</button>
         </form>
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
     
       <main>
