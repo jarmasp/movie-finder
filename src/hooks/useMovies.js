75ff3a8a27dc61de6cc2811282162111ac9527c8
Jose Armas <armasjose606@gmail.com>
2023-07-16 12:29:41 -0400

    Add: loading state

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -5,11 +5,21 @@ import { searchMovies } from '../services/getMovies'
 
 export const useMovies = ({query}) => {
   const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState([])
+  const [error, setError] = useState([])
   
   const getMovies = async () => {
-    const queriedMovies = await searchMovies({ query })
-    setMovies(queriedMovies)
+    try { 
+      setLoading(true)
+      setError(null)
+      const queriedMovies = await searchMovies({ query })
+      setMovies(queriedMovies)
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      setLoading(false)
+    }
   }
 
-    return {movies, getMovies}
-  }
+  return { movies, getMovies, loading, error }
+}
