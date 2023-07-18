7e2b8b687f620ea364bc0ac2a45914bddade378b
Jose Armas <armasjose606@gmail.com>
2023-07-18 17:19:38 -0400

    Perf: add useMemo hook to stop unnecessary rendering

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,25 +1,32 @@
-import { useState } from 'react'
+import { useState, useRef, useMemo } from 'react'
 // import withResults from '../mock/with-results.json'
 // import withoutResults from '../mock/without-results.json'
 import { searchMovies } from '../services/getMovies' 
 
-export const useMovies = ({query}) => {
+export const useMovies = ({ query }) => {
   const [movies, setMovies] = useState([])
-  const [loading, setLoading] = useState([])
-  const [error, setError] = useState([])
+  const [loading, setLoading] = useState([false])
+  const [error, setError] = useState([null])
+  const previousQuery = useRef(query)
   
-  const getMovies = async () => {
-    try { 
-      setLoading(true)
-      setError(null)
-      const queriedMovies = await searchMovies({ query })
-      setMovies(queriedMovies)
-    } catch (e) {
-      setError(e.message)
-    } finally {
-      setLoading(false)
+  const getMovies = useMemo(() => {
+    return async ({ query }) => {
+      if (query === previousQuery.current)
+        return
+    
+      try {
+        setLoading(true)
+        setError(null)
+        previousQuery.current = query
+        const queriedMovies = await searchMovies({ query })
+        setMovies(queriedMovies)
+      } catch (e) {
+        setError(e.message)
+      } finally {
+        setLoading(false)
+      }
     }
-  }
+  }, [])
 
   return { movies, getMovies, loading, error }
 }
